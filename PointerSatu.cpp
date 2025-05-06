3418eaffbac751b084c27fe0a4de817e23435929
DrivandiPratama <dripandipratama@gmail.com>
2025-05-06 16:03:05 +0700

    Membuat class pointer satu tidak error

--- PointerSatu.cpp
+++ PointerSatu.cpp
@@ -9,3 +9,16 @@ class mahasiswa{
     }
 };
 
+int main (){
+    mahasiswa mhs{1}; // objek mhs
+    mhs.showNim(); //member Access Operator
+
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //member Access Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //pointer Dereference pMhs
+    pMhs->nim = 3; //Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
